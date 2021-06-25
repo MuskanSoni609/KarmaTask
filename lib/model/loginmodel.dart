421b75a32a5b221e0ae3c038e7de818b0345e03e
MuskanSoni609 <60544009+MuskanSoni609@users.noreply.github.com>
2021-06-25 04:21:56 -0700

    Add files via upload

---
+++ lib/model/loginmodel.dart
@@ -0,0 +1,33 @@
+class LoginResponseModel {
+  final String token;
+  final String error;
+
+  LoginResponseModel({this.token, this.error});
+
+  factory LoginResponseModel.fromJson(Map<String, dynamic> json) {
+    return LoginResponseModel(
+        token: json["token"] != null ? json["token"] : "",
+        error: json["error"] != null ? json["error"] : "");
+  }
+}
+
+class LoginRequestModel {
+  String name;
+  String email;
+  String password;
+
+  LoginRequestModel({
+    this.name,
+    this.email,
+    this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = {
+      'name': name.trim(),
+      'email': email.trim(),
+      'password': password.trim(),
+    };
+    return map;
+  }
+}
